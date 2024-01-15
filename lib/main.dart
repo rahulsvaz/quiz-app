fe64d20e52acd6b38123a2393b96780bf666cda2
Rahul P <128817814+rahulsvaz@users.noreply.github.com>
2024-01-15 14:25:18 +0530

    Update main.dart
    Payment integration ui created

--- lib/main.dart
+++ lib/main.dart
@@ -13,3 +13,31 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(home: QuizScreen());
   }
 }
+
+
+import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/quiz_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: QuizScreen());
+  }
+}
+
+
+
+class PaymentPlaystore extends StatelessWidget{
+const PaymentPlaystore ({super. key});
+
+  @override
+  Widget build (BuildContext ctz){
+    return Container(child:Text("PAYMENT COMPLETED ");
+  }
+}
